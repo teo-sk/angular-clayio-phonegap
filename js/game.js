121faf3a19a6b3680ecea819bc302988c81590a4
Matej Zilak <teo@teo.sk>
2013-05-26 15:04:40 +0200

    game guessing logic

--- js/game.js
+++ js/game.js
@@ -7,11 +7,13 @@ function GameCtrl($scope) {
   //defaults
 
   $scope.gameState.started = false;
+  $scope.gameState.ended = false;
   $scope.gameState.settings = {};
   $scope.gameState.settings.min = 100;
   $scope.gameState.settings.max = 200;
   $scope.gameState.settings.attempts = 10;
   $scope.gameState.attempts = 0;
+  $scope.gameState.debug = false;
 
   $scope.startGame = function() {
     $scope.gameState.started = true;
@@ -22,5 +24,30 @@ function GameCtrl($scope) {
     var min = parseInt($scope.gameState.settings.min);
     var max = parseInt($scope.gameState.settings.max);
     $scope.gameState.number = min + Math.floor(Math.random() * (max - min + 1)); 
+    $scope.gameState.guess = Math.floor(min + (max - min) / 2);
+  }
+
+  $scope.guess = function() {
+    if ($scope.gameState.number == $scope.gameState.guess) {
+      $scope.gameState.won = true;
+      $scope.gameState.ended = true;
+      $scope.gameState.hint = "It was " + $scope.gameState.number;
+      $scope.gameState.finalScore = $scope.gameState.multiplyer * ($scope.gameState.settings.attempts - $scope.gameState.attempts)
+    } else {
+      if ($scope.gameState.number < $scope.gameState.guess) {
+        $scope.gameState.hint = "Try a little bit lower";
+      } else {
+        $scope.gameState.hint = "Try a little bit higher";
+      }
+    }
+
+    $scope.gameState.attempts++;
+
+    if ($scope.gameState.attempts == $scope.gameState.settings.attempts) {
+      $scope.gameState.lost = true;
+      $scope.gameState.hint = "It was " + $scope.gameState.number;
+      $scope.gameState.ended = true;
+    }
+
   }
 }
