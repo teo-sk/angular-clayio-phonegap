ed4f058b5a0eff8ab812458fd047ebe33cdf071c
Matej Zilak <teo@teo.sk>
2013-05-26 12:14:17 +0200

    added start game calculations

--- js/game.js
+++ js/game.js
@@ -11,8 +11,14 @@ function GameCtrl($scope) {
   $scope.gameState.settings.min = 100;
   $scope.gameState.settings.max = 200;
   $scope.gameState.settings.attempts = 10;
+  $scope.gameState.attempts = 0;
 
   $scope.startGame = function() {
     $scope.gameState.started = true;
+
+    //calculate score multiplyer
+    $scope.gameState.multiplyer = ($scope.gameState.settings.max - $scope.gameState.settings.min) / $scope.gameState.settings.attempts;
+    //generate number
+    $scope.gameState.number = $scope.gameState.settings.min + Math.floor(Math.random() * ($scope.gameState.settings.max - $scope.gameState.settings.min + 1)); 
   }
 }
